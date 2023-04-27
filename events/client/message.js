d0e9974eedbe0476497b049e264731f298532a0b
seangjr <sean@relampagos.org>
2023-04-27 15:39:12 +0800

    .

--- events/client/message.js
+++ events/client/message.js
@@ -19,51 +19,28 @@ module.exports = {
             "smelly",
             "monkey",
         ];
-        // schema creation
-        let profileData;
-        try {
-            profileData = profileSchema.findOne({
-                userID: message.author.id,
-            });
-            if (!profileData) {
-                let profile = await profileSchema.create({
-                    userID: message.author.id,
-                    guildID: message.guild.id,
-                    strikes: 0,
-                });
-                profile.save();
-            }
-        } catch (err) {
-            console.log(err);
-        }
 
-        // const ashtonsProfile = await profileSchema.findOne({
-        //     userID: "452794023351025685",
-        // });
-
-        if (message.author.bot) return;
-
-        const containsBannedWord = bannedWords.some((word) =>
-            message.content.toLowerCase().includes(word),
-        );
-
-        if (containsBannedWord) {
-            // ashtonsProfile.strikes += 1;
-            // await ashtonsProfile.save();
-            profileData.strikes += 1;
-            await profileData.save();
-            message.reply(
-                `⚒️ You have been given a strike for using a banned word. **Total strikes: ${ashtonsProfile.strikes}**`,
+        const content = message.content.toLowerCase();
+        if (bannedWords.some((word) => content.includes(word))) {
+            const userId = message.author.id;
+            const update = { $inc: { strikes: 1 } };
+            const options = { new: true, upsert: true };
+            const profile = await profileSchema.findOneAndUpdate(
+                { userId },
+                update,
+                options,
             );
-        }
 
-        const channelId = "1100298804940505108";
-        const channel = client.channels.cache.get(channelId);
-        if (strikes[message.author.id] >= 10) {
-            // channel.send(
-            //     "@everyone ASHTON IS A RACIST. :ninja: :ninja: :ninja: 🐒 🐒 🧑🏿",
-            // );
-            channel.send("test msg from cat bus bot");
+            if (profile.strikes >= 10) {
+                const channelId = "1100298804940505108";
+                const channel = message.guild.channels.cache.get(channelId);
+                channel.send(
+                    `<@${userId}> is a racist sia. :ninja: :ninja: :ninja:`,
+                );
+
+                profile.strikes = 0;
+                await profile.save();
+            }
         }
     },
 };
